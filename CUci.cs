65ed421f9f937a6b5a762d9f61594d3f49b8e5d1
Thibor <thibor@vp.pl>
2021-02-12 13:10:52 +0000

    Add project files.

---
+++ CUci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NSUci
+{
+	class CUci
+	{
+		public string command;
+		public string[] tokens;
+
+		public int GetIndex(string key, int def)
+		{
+			for (int n = 0; n < tokens.Length; n++)
+			{
+				if (tokens[n] == key)
+				{
+					return n + 1;
+				}
+			}
+			return def;
+		}
+
+		public int GetInt(string key, int def)
+		{
+			for (int n = 0; n < tokens.Length - 1; n++)
+			{
+				if (tokens[n] == key)
+				{
+					return Int32.Parse(tokens[n + 1]);
+				}
+			}
+			return def;
+		}
+
+		public void SetMsg(string msg)
+		{
+			if (msg == null)
+				msg = "";
+			tokens = msg.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+			if (tokens.Length > 0)
+				command = tokens[0];
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using NSUci;
+using NSChess;
+
+namespace BookReaderCdb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+			bool getMove = true;
+			List<string> movesEng = new List<string>();
+			CChess Chess = new CChess();
+			CUci Uci = new CUci();
+			string ax = "-ef";
+			List<string> listEf = new List<string>();
+			List<string> listEa = new List<string>();
+			for (int n = 0; n < args.Length; n++)
+			{
+				string ac = args[n];
+				switch (ac)
+				{
+					case "-ef":
+					case "-ea":
+						ax = ac;
+						break;
+					default:
+						switch (ax)
+						{
+							case "-ef":
+								listEf.Add(ac);
+								break;
+							case "-ea":
+								listEa.Add(ac);
+								break;
+						}
+						break;
+				}
+			}
+			string script = "http://www.chessdb.cn/cdb.php";
+			string engine = String.Join(" ", listEf);
+			string arguments = String.Join(" ", listEa);
+			Process myProcess = new Process();
+			if (File.Exists(engine))
+			{
+				myProcess.StartInfo.FileName = engine;
+				myProcess.StartInfo.WorkingDirectory = Path.GetDirectoryName(engine);
+				myProcess.StartInfo.UseShellExecute = false;
+				myProcess.StartInfo.RedirectStandardInput = true;
+				myProcess.StartInfo.Arguments = arguments;
+				myProcess.Start();
+			}
+			else
+			{
+				if (engine != "")
+					Console.WriteLine("info string missing engine");
+				engine = "";
+			}
+			if (script == "")
+			{
+				Console.WriteLine("info string missing script");
+				return;
+			}
+
+			string GetMove(string fen)
+			{
+				var col = new NameValueCollection
+				{
+					{ "action","querybest" },
+					{ "board", fen}
+				};
+				byte[] data;
+				try
+				{
+					data = new WebClient().UploadValues(@script, col);
+				}
+				catch
+				{
+					return "";
+				}
+				string msg = Encoding.UTF8.GetString(data);
+				string[] tokens = msg.Split(':');
+				if (tokens.Length > 1) {
+					string umo = tokens[1].Substring(0,4);
+					if (Chess.IsValidMoveUmo(umo, out _))
+						return umo;
+				}
+				return "";
+			}
+
+			while (true)
+			{
+				string msg = Console.ReadLine();
+				Uci.SetMsg(msg);
+				if ((Uci.command != "go") && (engine != ""))
+					myProcess.StandardInput.WriteLine(msg);
+				switch (Uci.command)
+				{
+					case "ucinewgame":
+						getMove = true;
+						break;
+					case "position":
+						string fen = "";
+						int lo = Uci.GetIndex("fen", 0);
+						int hi = Uci.GetIndex("moves", Uci.tokens.Length);
+						if (lo > 0)
+						{
+							if (lo > hi)
+								hi = Uci.tokens.Length;
+							for (int n = lo; n < hi; n++)
+							{
+								if (n > lo)
+									fen += ' ';
+								fen += Uci.tokens[n];
+							}
+						}
+						Chess.SetFen(fen);
+						lo = Uci.GetIndex("moves", 0);
+						hi = Uci.GetIndex("fen", Uci.tokens.Length);
+						if (lo > 0)
+						{
+							if (lo > hi)
+								hi = Uci.tokens.Length;
+							for (int n = lo; n < hi; n++)
+							{
+								string m = Uci.tokens[n];
+								Chess.MakeMove(Chess.UmoToEmo(m));
+							}
+						}
+						break;
+					case "go":
+						string move = "";
+						if (getMove)
+						{
+							move = GetMove(Chess.GetFen());
+							getMove = move != "";
+						}
+						if (getMove)
+							Console.WriteLine($"bestmove {move}");
+						else if (engine == "")
+							Console.WriteLine("enginemove");
+						else
+							myProcess.StandardInput.WriteLine(msg);
+						break;
+				}
+			}
+		}
+	}
+}
